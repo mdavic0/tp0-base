037f6580cdf2f0196b256b75735fe95d0db7d394
Mauricio Davico <mdavico@fi.uba.ar>
2025-03-23 01:19:00 -0300

    fix operator

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,15 +2,14 @@
 
 # Configuración
 SERVER_HOSTNAME="server"
-NETWORK_NAME="tp0_testing_net" 
+NETWORK_NAME="tp0_testing_net"
 TEST_MESSAGE="Hello, EchoServer!"
 EXPECTED_RESPONSE="$TEST_MESSAGE"
 SERVER_PORT=12345
 
-
 ACTUAL_RESPONSE=$(docker run --rm --network "$NETWORK_NAME" busybox:latest sh -c "echo '$TEST_MESSAGE' | nc -w 5 $SERVER_HOSTNAME $SERVER_PORT")
 
-if [ "$ACTUAL_RESPONSE" == "$EXPECTED_RESPONSE" ]; then
+if [ "$ACTUAL_RESPONSE" = "$EXPECTED_RESPONSE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
