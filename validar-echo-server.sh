1c9cf31a34a519fca9598bb4a5aa941106a24b01
Mauricio Davico <mdavico@fi.uba.ar>
2025-03-23 01:04:19 -0300

    fix

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,16 @@
+#!/bin/sh
 
+# Mensaje de prueba
 MESSAGE="Hello, server!"
 
-RESPONSE=$(docker run --rm --network tp0_testing_net alpine/netcat server 12345 <<< "$MESSAGE")
+# Ejecutar netcat dentro de un contenedor de Alpine
+RESPONSE=$(docker run --rm --network tp0_testing_net alpine/netcat server 12345 <<EOF
+$MESSAGE
+EOF
+)
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+# Comparar la respuesta con el mensaje enviado
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
